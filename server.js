88aaeb82f4f10eccf79c1dc03e8c2fce68e650b1
FilipeLacerda738 <filipelacerda122@gmail.com>
2025-11-28 12:31:53 -0300

    Debugs para o render

--- server.js
+++ server.js
@@ -12,11 +12,17 @@ app.get('/api/weather', async (req, res) => {
     const city = req.query.city;
     const apiKey = process.env.API_KEY;
 
+    
+    console.log(`[LOG RENDER] Recebi solicitação do Front-End para cidade: ${city} às ${new Date().toLocaleTimeString()}`);
+    
+
     if (!city) {
         return res.status(400).json({ error: 'Cidade não fornecida' });
     }
 
     if (!apiKey) {
+        
+        console.error("[ERRO CRÍTICO] API Key não encontrada no .env");
         return res.status(500).json({ error: 'Configuração de API Key ausente' });
     }
 
@@ -27,12 +33,14 @@ app.get('/api/weather', async (req, res) => {
         const data = await apiResponse.json();
 
         if (data.cod && data.cod !== 200) {
+            console.log(`[API ERRO] OpenWeather respondeu com erro: ${data.message}`);
             return res.status(parseInt(data.cod)).json(data);
         }
 
         res.status(apiResponse.status).json(data);
 
     } catch (error) {
+        console.error("[ERRO INTERNO]", error);
         res.status(500).json({ error: 'Erro interno no servidor proxy' });
     }
 });
